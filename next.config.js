904d6cb29c157966d44336dc7d8d295c0a19f842
Arif Hidayat <moharifhidayat7@outlook.com>
2021-11-25 11:42:57 +0700

    sas

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ module.exports = {
       'picsum.photos',
       'via.placeholder.com',
       'fonts.googleapis.com',
+      'fonts.gstatic.com',
       'rumahdigitalgombengsari.com',
       '10.0.0.3',
       'localhost',
