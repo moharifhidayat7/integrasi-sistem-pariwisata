cb70e57571fdc4053cef4df229369fdffa4d15cb
Arif Hidayat <moharifhidayat7@outlook.com>
2021-11-25 11:34:27 +0700

    ee

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['picsum.photos', 'via.placeholder.com', '10.0.0.3', 'localhost'],
+    domains: [
+      'picsum.photos',
+      'via.placeholder.com',
+      'fonts.googleapis.com',
+      'rumahdigitalgombengsari.com',
+      '10.0.0.3',
+      'localhost',
+    ],
   },
 }
